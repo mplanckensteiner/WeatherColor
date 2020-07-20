c2506fe3348573a054013f9f143f3bb5858f5c7c
Miguel Planckensteiner <49713385+mikeplanck@users.noreply.github.com>
2020-07-20 13:48:03 +0200

    Initial Commit

---
+++ Weather App 2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App 2020
+//
+//  Created by Miguel Planckensteiner on 20.07.20.
+//  Copyright © 2020 Miguel Planckensteiner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
